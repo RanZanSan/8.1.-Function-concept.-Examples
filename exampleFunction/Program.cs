c5353b8f6beb79fdfff5c11fdffa37af4139e0ab
Andrey Rasskazov <ranzansan@gmail.com>
2021-09-17 17:48:37 +0300

    update

---
+++ exampleFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleFunction
+{
+    class Program
+    {
+        static int Factorial(int number)
+        {
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            int factN = Factorial(n);
+            int factK = Factorial(k);
+            int nSubtractK = n - k;
+            int factNSubtractK = Factorial(nSubtractK);
+
+            Console.WriteLine(factN / (factK * factNSubtractK));
+            Console.ReadKey();
+        }
+    }
+}
